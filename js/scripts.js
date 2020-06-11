82f413bb0a0daf1fa5e19884f120f133ec42d20f
Nikolas Kososik <nikolas.kososik@gmail.com>
2020-06-11 10:37:08 -0700

    complete spec to gather user input
    co-authored-by: Micheal Hansen <micheal.hansen@gmail.com>
    Co-authored by: Michael Petersen <michael_petersen_email@yahoo.com>

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $("form#numeral").submit(function(event) {
+    event.preventDefault();
+    const numberInput = parseInt($("input[name=number]").val());
+
+    
+  })
+})
